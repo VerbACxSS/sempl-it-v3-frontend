3590fcb6b8854ceedf78863c37f1343c04747636
RedHitMark <russodivito.marco@gmail.com>
2025-11-07 21:02:42 +0100

    Add video tutorial in homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
 import {AfterViewInit, Component} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
-import {ItCardComponent} from 'design-angular-kit';
+import {ItButtonDirective, ItCardComponent, ItCollapseComponent} from 'design-angular-kit';
 import {SeoService} from '../../services/seo.service';
+import {TutorialModalComponent} from '../../components/modals/tutorial-modal/tutorial-modal.component';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,14 @@ import {SeoService} from '../../services/seo.service';
   templateUrl: './home.component.html',
   imports: [
     ItCardComponent,
-    NgOptimizedImage
+    NgOptimizedImage,
+    TutorialModalComponent,
+    ItButtonDirective,
+    ItCollapseComponent
   ]
 })
 export class HomeComponent implements AfterViewInit {
+  public readonly VIDEO_URL = "https://drive.google.com/file/d/1dal9thz7htgUWo0yLYyprZOp3JPXz-hK/preview"
 
   constructor(private seoService: SeoService) {
   }
